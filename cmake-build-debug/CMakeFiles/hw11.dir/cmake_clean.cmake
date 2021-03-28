7366f43f2b10979da4b2ae985435a67f6cc035a3
Ike Ofoegbu <ike12346@gmail.com>
2021-03-27 21:16:17 -0700

    First commit

---
+++ cmake-build-debug/CMakeFiles/hw11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw11.dir/main.cpp.o"
+  "hw11"
+  "hw11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
